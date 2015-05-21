f79f47955bc5203aff765027ec32db5d2d0c9b9c
Ross McKee <rossrobertmckee@gmail.com>
2015-05-21 20:04:15 +0000

    Validation

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,6 @@
 class Place < ActiveRecord::Base
 	belongs_to :user
-	validates :name, :presence => true
+	validates :name, :presence => true, :uniqueness => true, :length => { :maximum => 120, :minimum => 3 }
+	validates :address, :presence => true
+	validates :description, :presence => true,  :uniqueness => true, :length => { :maximum => 300 }
 end
