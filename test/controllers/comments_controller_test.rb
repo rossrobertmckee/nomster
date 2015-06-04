c8d6653a553bf6c41db6ceb35af808dd7f8e467f
Ross McKee <rossrobertmckee@gmail.com>
2015-06-04 13:27:52 +0000

    Moddified code - Test 3 still doesn't work

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -2,13 +2,14 @@ require 'test_helper'
 require 'factory_girl_rails'
 
 class CommentsControllerTest < ActionController::TestCase
-	
-
-	test "comment_create" do  
+	setup do  
  		user = FactoryGirl.create(:user)
  		sign_in user
     	place = FactoryGirl.create(:place)
 		comment = FactoryGirl.create(:comment, user: user, place: place)
+	end
+
+	test "comment_me" do
     	assert_equal 1, place.comment.count    
     	assert_redirected_to places_path(place)
   end
--- test/factories/comment.rb
+++ test/factories/comment.rb
@@ -3,4 +3,8 @@ FactoryGirl.define do
   	message "Please work!!!"
   	rating "5_stars"
   end
+
+  factory :email do
+  	email "test@test.com"
+  end
 end
