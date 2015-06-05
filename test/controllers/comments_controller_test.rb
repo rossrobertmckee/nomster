11a5794bf89b90e62f4a26fe66a6aeb1445e1b68
Ross McKee <rossrobertmckee@gmail.com>
2015-06-05 00:01:14 +0000

    Test almost done

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -5,10 +5,9 @@ class CommentsControllerTest < ActionController::TestCase
 	test "comment_create" do  
  		user = FactoryGirl.create(:user)
  		sign_in user
-    	place = FactoryGirl.create(:place)
-		comment = FactoryGirl.create(:comment, place: place)
+    	place = FactoryGirl.create(:place, :user => user)
+		comment = FactoryGirl.create(:comment, user: user, place: place)
     	assert_not_nil comment    
     	assert_redirected_to place_path(place)
-  end
-
+	end
 end
--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -17,3 +17,4 @@ class CommentTest < ActiveSupport::TestCase
   end
 
 end
+ 
